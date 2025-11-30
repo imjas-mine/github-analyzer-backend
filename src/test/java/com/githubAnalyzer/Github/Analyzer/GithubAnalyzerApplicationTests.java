91ab4030a28476e8e93a3f4a164f00fa31ade0e5
jasmine <jasminkaur5858@gmail.com>
2025-11-30 00:12:09 -0500

    first commit

---
+++ src/test/java/com/githubAnalyzer/Github/Analyzer/GithubAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubAnalyzer.Github.Analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
